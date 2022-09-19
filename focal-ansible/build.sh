e37fdd2e1153f58993e5203c32b10b0d4ce6db75
Slava <slava@parsehub.com>
2022-09-19 10:37:44 -0400

    Fix focal-ansible docker driver failure

--- focal-ansible/build.sh
+++ focal-ansible/build.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$script_dir"/vars
-docker build -t "$FOCAL_ANSIBLE_IMG_NAME" .
+docker build --no-cache -t "$FOCAL_ANSIBLE_IMG_NAME" .
 docker tag "$FOCAL_ANSIBLE_IMG_NAME" "$FOCAL_ANSIBLE_IMG_LATEST"
 docker tag "$FOCAL_ANSIBLE_IMG_NAME" "$FOCAL_ANSIBLE_IMG_VERSIONED"
 
