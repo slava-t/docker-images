d96f824ed5fd1931b2e75b2ed29afadcb68f73d5
Slava <slava@parsehub.com>
2023-11-01 09:15:12 -0400

    Create a docker image for ansible and fabric

---
+++ fabansible/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "$script_dir"/vars
+docker build --no-cache -t "$FABANSIBLE_IMG_VERSIONED" .
+
