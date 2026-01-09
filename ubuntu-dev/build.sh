a94b7abf38fbf571781897aaf2d9c0de73945b94
Slava <slava@parsehub.com>
2026-01-09 11:38:19 -0500

    ubuntu-dev 0.0.2

--- ubuntu-dev/build.sh
+++ ubuntu-dev/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. "$script_dir"/vars
-docker build -t "$UBUNTU_DEV_IMG_NAME" .
+distribution=${1:-24}
+. "$script_dir"/vars "$distribution"
+docker build -f Dockerfile"$distribution" -t "$UBUNTU_DEV_IMG_NAME" .
 docker tag "$UBUNTU_DEV_IMG_NAME" "$UBUNTU_DEV_IMG_LATEST"
 docker tag "$UBUNTU_DEV_IMG_NAME" "$UBUNTU_DEV_IMG_VERSIONED"
 
--- ubuntu-dev/push.sh
+++ ubuntu-dev/push.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. "$script_dir"/vars
+distribution=${1:-24}
+. "$script_dir"/vars "$distribution"
 docker push "$UBUNTU_DEV_IMG_LATEST"
 docker push "$UBUNTU_DEV_IMG_VERSIONED"
 
